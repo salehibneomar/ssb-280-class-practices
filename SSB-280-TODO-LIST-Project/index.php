a3f19b80d17de7f63cdcb6b1ff28694d416a6b93
Saleh <salehibneomar@gmail.com>
2020-08-06 16:21:07 +0600

    Major updates.

--- SSB-280-TODO-LIST-Project/index.php
+++ SSB-280-TODO-LIST-Project/index.php
@@ -48,6 +48,13 @@
     </section>
     <!-- Main section ends -->
 
+    <footer class="footer">
+        &copy; 2020, Saleh Ibne Omar <br>
+        <?php 
+        echo $_SERVER['REMOTE_ADDR']; ?>
+    </footer>
+
+
     <!-- Form modal starts -->
     <form class="ui small modal form-modal" method="POST">
         <div class="header" id="form-title"></div>
@@ -75,215 +82,40 @@
     </form>
     <!-- Form modal ends -->
 
-<div class="ui mini modal" id="message-modal">
-</div>
+    <!-- Mini Modals -->
+    <div class="ui mini modal" id="message-modal">
+    </div>
 
-<div class="ui mini modal" id="confirmation-modal">
-    <div class="header">Confirmation&ensp;<i class="exclamation teal icon"></i></div>
-    <div class="content">
-        <p id="confirm-msg-details">Are you sure? you won't be able to revert this!</p>
+    <div class="ui mini modal" id="confirmation-modal">
+        <div class="header">Confirmation&ensp;<i class="exclamation teal icon"></i></div>
+        <div class="content">
+            <p id="confirm-msg-details">Are you sure? you won't be able to revert this!</p>
+        </div>
+        <div class="actions">
+            <div class="ui cancel red button">No</div>
+            <button type="button" class="ui green button" id="confirm-btn">Yes</button>
+        </div>
     </div>
-    <div class="actions">
-        <div class="ui cancel red button">No</div>
-        <button type="button" class="ui green button" id="confirm-btn">Yes</button>
-    </div>
-</div>
+    <!-- Mini Modals -->
+
 
 <!-- Script Tags -->
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js"></script>
     <script>
-
-    $(window).on('load', function () {
-        setTimeout(() => {
-            $('#loader').fadeOut('slow', function () {
-                $('#loader').remove();
-            });
+        $(window).on('load', function () {
+            setTimeout(() => {
+                $('#loader').fadeOut('slow', function () {
+                    $('#loader').remove();
+                });
     
-            $('#main-section').fadeIn('slow', function () {
-                $('#main-section').show();
-            }); 
-        }, 400);
-    });
-
-    $(document).ready(function(){
-
-//======================= CRUD Ajax functions =======================
-
-        function readAll(){
-            $.ajax({
-                url:     "operations/readAll.php",
-                method:  "POST",
-                success: function(data){
-                    setTimeout(() => {
-                        $('#data-table').html(data);
-                    }, 600);
-                },
-                error:  function(){
-                    $('#data-table').html('<p class="center aligned">Network/Server error: Data fetching failed!</p>');
-                }
-            });
-        }
-
-        function createOrUpdate(url, data){
-            $.ajax({
-                url:     url,
-                method:  "POST",
-                data:    data,
-                success: function(returnedData){
-                    setTimeout(() => {
-                        $('#message-modal').modal('show');
-                        $('#message-modal').html(returnedData);
-                        //========= Ajax Function call =========
-                        readAll();
-                    }, 500);
-                },
-                error:  function(){
-                    $('#input-err').text("Network/Server error: Couldn't perform operation!");;
-                }
-            });
-        }
-
-        function readById(id){
-            $.ajax({
-                url: "operations/readById.php",
-                method: "POST",
-                data: 'id='+id,
-                success: function(data){
-                    var data = JSON.parse(data);
-                    if(data==null){
-                        //========= Ajax Function call =========
-                        readAll();
-                    }
-                    else{
-                        $('#work-id').val(id);
-                        $('#workName').val(data.workName);
-                        $('#fromDate').val(data.fromDate);
-                        $('#toDate').val(data.toDate);
-                    }
-                },
-                error:  function(){
-                    $('#input-err').text("Network/Server error: Couldn't fetch data!");
-                    $('#form-submit-btn').addClass("disabled");
-                }
-            });
-        }
-
-        function deleteWork(id){
-            $.ajax({
-                url:     "operations/delete.php",
-                method:  "POST",
-                data:    'id='+id,
-                success: function(data){
-                    setTimeout(() => {
-                        $('#message-modal').modal('show');
-                        $('#message-modal').html(data);
-                        //========= Ajax Function call =========
-                        readAll();
-                    }, 500);
-                },
-                error:  function(){
-                    $('#confirm-btn').addClass("loading");
-                    $('#confirm-msg-details').css("color", "red");
-                    $('#confirm-msg-details').text("Network/Server error: Couldn't perform operation!");
-                }
-            });
-        }
-
-//======================= On Click Functions =======================
-
-        $('#add-to-list-btn').on('click', function(){
-            $('.form-modal').modal('show');
-            $('#form-title').text("Add To List");
-            $('#form-submit-btn').text("Submit");
-            $('#form-submit-btn').attr("name", "add-to-list");
-            $('#submit-type').attr("name", "create");
+                $('footer, section').fadeIn('slow', function () {
+                    $('footer, section').show();
+                }); 
+            }, 400);
         });
-
-        $(document).on('click', '.work-edit-btn', function(){
-            $('.form-modal').modal('show');
-            $('#form-title').text("Update Work");
-            $('#form-submit-btn').text("Update");
-            $('#form-submit-btn').attr("name", "update-work");
-            $('#submit-type').attr("name", "update");
-            //========= Ajax Function call =========
-            readById($(this).val());
-        });
-
-        $(document).on('click', '.work-delete-btn', function(){
-            var id = $(this).val();
-            $('#confirmation-modal').modal('show');
-            $('#confirm-btn').on("click", function(){
-                $('#confirm-btn').addClass("loading disabled");
-                //========= Ajax Function call =========
-                deleteWork(id);
-            });
-        });
-
-
-        $('.form-modal').modal({
-            onHide: function(){
-                $('#input-err').text("");
-                $('form').find('input').val("");
-                $('#form-submit-btn').removeClass("loading");
-                setTimeout(() => {
-                    $('#form-submit-btn').removeClass("disabled");
-                }, 500);
-            }
-        });
-
-        $('#confirmation-modal').modal({
-            onHide: function(){
-                $('#confirm-btn').removeClass("loading");
-                setTimeout(() => {
-                    $('#confirm-btn').removeClass("disabled");
-                    $('#confirm-msg-details').css("color", "black");
-                    $('#confirm-msg-details').text("Are you sure? you won't be able to revert this!");
-                }, 500);
-            }
-        });
-
-//======================= Form Submission Functions =======================
-
-        $('#form-submit-btn').on('click', function(e){
-            e.preventDefault();
-            var workName    = $('#workName').val().trim();
-            var fromDate = new Date($('#fromDate').val());
-            var toDate   = new Date ($('#toDate').val());
-            
-            if(fromDate=="Invalid Date" || toDate=="Invalid Date" || workName==""){
-                $('#input-err').text("Empty/Invalid Input field found!");
-            }
-            else{
-                if(workName.length>200){
-                    $('#input-err').text("Work name cannot have more than 200 characters!");
-                }
-                else{
-                    var submitType = $('#form-submit-btn').attr("name");
-                    var url = "";
-                    if(submitType=="update-work"){
-                        url = "operations/update.php";
-                    }
-                    else if(submitType=="add-to-list"){
-                        url = "operations/create.php";
-                    }
-
-                    var data = $('form').serialize();
-                    $('#form-submit-btn').addClass("loading disabled");
-                    //========= Ajax Function call =========
-                    createOrUpdate(url, data);
-                    
-                }
-            }
-        });
-
-        //========= Ajax Function call =========
-
-        readAll();
-                    
-    });
-
     </script>
+    <script src="js/script.js"></script>
 </body>
 </html>
--- SSB-280-TODO-LIST-Project/operations/update.php
+++ SSB-280-TODO-LIST-Project/operations/update.php
@@ -2,28 +2,38 @@
 
     require '../config/config.php';
 
+    $execution = false;
+
     if(isset($_POST['update'])){
         $id         = $_POST['id'];
         $workName   = $_POST['workName'];
         $fromDate   = $_POST['fromDate'];
         $toDate     = $_POST['toDate'];
 
-        $query     = "UPDATE todolist SET workName='$workName', fromDate='$fromDate', toDate='$toDate' 
+        $query     = "UPDATE todolist SET workName='$workName', fromDate='$fromDate', toDate='$toDate', workStatus=0 
                       WHERE id='$id' LIMIT 1";
         $execution = mysqli_query($conn, $query);
-        
-        if($execution==true){
-            $msgDetails  = "Your work is updated successfully!";
-            echo operationMessage($GLOBALS['succMsgTitle'], $msgDetails);
-        }
-        else{
-            $msgDetails = "Error occured while updating, try again!";
-            echo operationMessage($GLOBALS['errMsgTitle'], $msgDetails);
-        }
+    
     }
+    else if(isset($_POST['workDone'])){
+        $id = $_POST['id'];
+
+        $query = "UPDATE todolist SET workStatus=1 WHERE id='$id' LIMIT 1";
+        $execution = mysqli_query($conn, $query);
+    }
+
+
+    if($execution==true){
+        $msgDetails  = "Your work is updated successfully!";
+        echo operationMessage($GLOBALS['succMsgTitle'], $msgDetails);
+    }
+    else{
+        $msgDetails = "Error occured while updating, try again!";
+        echo operationMessage($GLOBALS['errMsgTitle'], $msgDetails);
+    }
+
 
     mysqli_close($conn);
 
-    
 
 ?>
